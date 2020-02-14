0692c23ad19b95a57d873e45a55b1f352f90c963
Yannick <yannick.boogaerts@technifutur.be>
2020-02-14 14:27:13 +0100

    version initiale

---
+++ src/be/technifutur/designpatterns/supercanard/Main.java
@@ -0,0 +1,14 @@
+package be.technifutur.designpatterns.supercanard;
+
+class Main {
+
+    public static void main(String[] args) {
+        afficheTitre();
+    }
+
+    private static void afficheTitre() {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!     Super Canard     !!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
